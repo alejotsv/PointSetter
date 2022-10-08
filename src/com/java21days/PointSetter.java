e95d607e832d2397d6b329addbcbe52b935133b1
Alejandro Salas <asalas.wd@outlook.com>
2022-10-08 18:01:32 -0400

    Add methods to move location

--- src/com/java21days/PointSetter.java
+++ src/com/java21days/PointSetter.java
@@ -1,7 +1,57 @@
 package com.java21days;
 
+import java.awt.*;
+import java.util.Scanner;
+
 public class PointSetter {
     public static void main(String[] args) {
-        System.out.println("This is the point setter method");
+        Point location = new Point(0, 0);
+
+
+    }
+
+    static void readLocation(Point location) {
+        System.out.println("X: " + location.x);
+        System.out.println("Y: " + location.y);
+    }
+
+    static void moveRight(Point location) {
+        location.x++;
+    }
+
+    static void moveLeft(Point location) {
+        location.x--;
+    }
+
+    static void moveUp(Point location) {
+        location.y++;
+    }
+
+    static void moveDown(Point location) {
+        location.y--;
+    }
+
+    static void getDirections(Point location) {
+        boolean exit = false;
+        while(!exit) {
+            System.out.println("Where should we go?\n\r>");
+            Scanner scanner = new Scanner(System.in);
+            String direction = scanner.nextLine();
+
+            switch(direction) {
+                case "right":
+                case "r":
+                    moveRight(location);
+                case "left":
+                case "l":
+                    moveLeft(location);
+                case "up":
+                case "u":
+                    moveUp(location);
+                case "down":
+                case "d":
+                    moveDown(location);
+            }
+        }
     }
 }
